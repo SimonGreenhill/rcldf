df406a96678cd9490e82ec7a16b25d3a9ce15264
SimonGreenhill <simon@simon.net.nz>
2019-07-19 12:50:45 +0200

    adding get_citation utility

--- R/get_citation.R
+++ R/get_citation.R
@@ -2,6 +2,10 @@
 #'
 #' @param obj A CLDF object
 #' @return An (invisible) string containing the citation text
+#' @export
+#' @examples
+#' cldfobj <- cldf(system.file("extdata/huon", "cldf-metadata.json", package = "rcldf"))
+#' get_citation(cldfobj)
 get_citation <- function(obj){
     if (!inherits(obj, "cldf")) stop("'obj' must inherit from class cldf")
     cat(obj$metadata$`dc:bibliographicCitation`)
