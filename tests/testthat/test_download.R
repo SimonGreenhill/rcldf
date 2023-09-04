23d40193859ece1ef8985504059da375575c8a50
SimonGreenhill <SimonGreenhill@users.noreply.github.com>
2023-09-05 09:40:42 +1200

    reduce reuse

--- tests/testthat/test_download.R
+++ tests/testthat/test_download.R
@@ -1,17 +1,27 @@
+library(archive)
+library(openssl)
+
 test_that("test download errors on non-url", {
     expect_error(download("/path/to/file"), "Does not look like a URL")
 })
 
 
 test_that("test download", {
-    fakeurl <- "examples/wals_1A_cldf.zip"
+    fakeurl <- 'wals_1A_cldf.zip'
+
     tmpdir <- tempdir()
+
+    # create zip file
+    zipfile <- file.path(tmpdir, fakeurl)
+    archive::archive_write_dir(zipfile, 'examples/wals_1A_cldf')
+
     expected_staging_dir <- file.path(tmpdir, openssl::md5(fakeurl))
 
     mockthat::with_mock(
         # mock out curl download to copy file
-        `curl::curl_download` = function(url, tmp) file.copy(fakeurl, tmp),
-        `is_url` = function(...) TRUE,  # and patch this to return TRUE
+        # and patch is_url to return TRUE
+        `curl::curl_download` = function(url, tmp) file.copy(zipfile, tmp),
+        `is_url` = function(...) TRUE,
         staging_dir <- download(fakeurl, tmpdir)
     )
 
@@ -37,46 +47,5 @@ test_that("test download", {
             `is_url` = function(...) TRUE,  # and patch this to return TRUE
             staging_dir <- download(fakeurl, tmpdir)
         ), "Already downloaded")
-
-})
-
-
-test_that("test download creates cache_dir", {
-    fakeurl <- "examples/wals_1A_cldf.zip"
-    cache_dir = file.path(tempdir(), "missing")
-    expect_equal(dir.exists(cache_dir), FALSE)
-    mockthat::with_mock(
-        # mock out curl download to copy file
-        `curl::curl_download` = function(url, tmp) file.copy(fakeurl, tmp),
-        `is_url` = function(...) TRUE,  # and patch this to return TRUE
-        staging_dir <- download(fakeurl, cache_dir=cache_dir)
-    )
-    expect_equal(dir.exists(cache_dir), TRUE)
-})
-
-
-test_that("test download via CLDF", {
-    fakeurl <- "examples/wals_1A_cldf.zip"
-    tmpdir <- tempdir()
-    expected_staging_dir <- file.path(tmpdir, openssl::md5(fakeurl))
-
-    mockthat::with_mock(
-        # mock out curl download to copy file
-        `curl::curl_download` = function(url, tmp) file.copy(fakeurl, tmp),
-        `is_url` = function(...) TRUE,  # and patch this to return TRUE
-        obj <- cldf(fakeurl, cache_dir=expected_staging_dir)
-    )
-
-    for (derived in list.files(file.path(expected_staging_dir, 'wals_1A_cldf'), full.names=TRUE)) {
-        # these are the original ones used to make the zip file
-        original <- file.path('examples/wals_1A_cldf', basename(derived))
-        expect_equal(file.exists(original), TRUE)
-        expect_equal(file.exists(derived), TRUE)
-        expect_equal(
-            # use as.vector to lose the name
-            as.vector(tools::md5sum(original)),
-            as.vector(tools::md5sum(derived))
-        )
-    }
 
 })
