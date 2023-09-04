46e71d93541c0f04d653c840c434644e4e68b339
HedvigS <hedvig.skirgard@gmail.com>
2023-09-04 17:02:59 +0200

    Update add_family_name_column.R

--- R/add_family_name_column.R
+++ R/add_family_name_column.R
@@ -3,6 +3,7 @@
 #' @param Table data-frame of CLDF table with the columns  "Family_ID", "Name" and "Glottocode".
 #' @return data-frame with Family_name column.
 #' @note It is necessary that for every unique glottocode in Family_ID there is a row with a Glottocode and Name to match that. If there isn't, languages will have missing values for their Family_name even though they are not isolates.
+#'  If The current LanguageTable lacks the required columns, consider using a combination of the LanguageTable and ValueTable of glottolog-cldf.
 #' @export
 
 #Table <- LanguageTable
@@ -10,7 +11,7 @@
 add_family_name_column <- function(Table = NULL){
 
     if(!all(c("Family_ID", "Name", "Glottocode") %in% colnames(Table))){
-        stop("The Table needs to have all of these columns: Name, Glottocode and Family_ID.")
+        stop("The Table needs to have all of these columns: Name, Glottocode and Family_ID. If The current LanguageTable lacks the required columns, consider using a combination of the LanguageTable and ValueTable of glottolog-cldf.")
     }
 
     Table <- Table %>%
