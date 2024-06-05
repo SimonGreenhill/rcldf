186742a8b471c15d3a4d9600d5facb64c045919c
SimonGreenhill <SimonGreenhill@users.noreply.github.com>
2024-06-06 09:10:35 +1200

    ensure tests for resolve_path use tempdir

--- tests/testthat/test_resolve_path.R
+++ tests/testthat/test_resolve_path.R
@@ -4,22 +4,22 @@ test_that("resolve_path", {
 
     expected <- csvwr::read_metadata(path)
     # given json
-    expect_equal(resolve_path(path)$metadata, expected)
+    expect_equal(resolve_path(path, tempdir())$metadata, expected)
 
     # given dir
-    expect_equal(resolve_path('examples/wals_1A_cldf')$metadata, expected)
+    expect_equal(resolve_path('examples/wals_1A_cldf', tempdir())$metadata, expected)
     # dir with trailing slash
-    expect_equal(resolve_path('examples/wals_1A_cldf/')$metadata, expected)
+    expect_equal(resolve_path('examples/wals_1A_cldf/', tempdir())$metadata, expected)
 
     # given full path
     p <- base::normalizePath(path)
-    expect_equal(resolve_path(p)$metadata, expected)
+    expect_equal(resolve_path(p, tempdir())$metadata, expected)
     p <- base::normalizePath("examples/wals_1A_cldf")
-    expect_equal(resolve_path(p)$metadata, expected)
+    expect_equal(resolve_path(p, tempdir())$metadata, expected)
 
     # give dir with multiple jsons
     expect_equal(
-        resolve_path('examples/multiple_json')$metadata,
+        resolve_path('examples/multiple_json', tempdir())$metadata,
         csvwr::read_metadata('examples/multiple_json/valid.json')
     )
 
@@ -27,28 +27,28 @@ test_that("resolve_path", {
 
     # given invalid file
     expect_error(
-        resolve_path('examples/wals_1A_cld')$metadata,
+        resolve_path('examples/wals_1A_cld', tempdir())$metadata,
         "does not exist"
     )
     expect_error(
-        resolve_path('examples/bad/StructureDataset-metadata.json')$metadata,
+        resolve_path('examples/bad/StructureDataset-metadata.json', tempdir())$metadata,
         "does not exist"
     )
 
     expect_error(
-        resolve_path('examples/wals_1A_cldf/values.csv')$metadata,
+        resolve_path('examples/wals_1A_cldf/values.csv', tempdir())$metadata,
         "Need either"
     )
 
     # no metadata JSON file
     expect_error(
-        resolve_path("examples/not_a_cldf")$metadata,
+        resolve_path("examples/not_a_cldf", tempdir())$metadata,
         "no metadata JSON file found"
     )
 
     # multiple JSON files found
     expect_error(
-        resolve_path("examples/not_a_cldf/also_not_a_cldf")$metadata,
+        resolve_path("examples/not_a_cldf/also_not_a_cldf", tempdir())$metadata,
         "no metadata JSON file found"
     )
 })
@@ -56,7 +56,8 @@ test_that("resolve_path", {
 
 test_that("resolve_path handles archives (.zip)", {
     expected <- csvwr::read_metadata('examples/wals_1A_cldf/StructureDataset-metadata.json')
-    obtained <- resolve_path("examples/wals_1A_cldf.zip")
+    obtained <- resolve_path("examples/wals_1A_cldf.zip", tempdir())
+
     expect_equal(obtained$metadata, expected)
 
     # check a table at random
