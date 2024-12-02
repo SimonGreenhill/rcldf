ae88b57ec742ba7182f2ca59b72cd0b925e17d2a
SimonGreenhill <SimonGreenhill@users.noreply.github.com>
2024-12-02 14:38:30 +1300

    remove citation for namespace clashes

--- tests/testthat/test_citation.R
+++
@@ -1,9 +0,0 @@
-test_that("test summary.cldf", {
-
-    expect_error(citation('x'), "'object' must inherit from class cldf")
-
-    df <- cldf("examples/wals_1A_cldf/StructureDataset-metadata.json")
-    out <- capture.output(citation(df))
-
-    expect_match(out[1], "Cite me like this!")
-})
--- tests/testthat/test_get_citation.R
+++
@@ -1,6 +0,0 @@
-test_that("test get_citation", {
-    expect_error(get_citation('x'), "'obj' must inherit from class cldf")
-    df <- cldf("examples/wals_1A_cldf/StructureDataset-metadata.json")
-    out <- capture.output(get_citation(df))
-    expect_match(out[1], "Cite me like this!")
-})
