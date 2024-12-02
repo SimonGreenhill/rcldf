648961766f1db94e447e97beec1da44dc0989f6c
SimonGreenhill <SimonGreenhill@users.noreply.github.com>
2024-12-02 14:39:17 +1300

    robustify get_details

--- R/get_details.R
+++ R/get_details.R
@@ -1,16 +1,15 @@
-
 #' Returns a dataframe of with details on the CLDF dataset in `path`.
 #' @param path the path to resolve
 #' @export
 #' @return A dataframe.
 get_details <- function(path) {
     md <- rcldf::resolve_path(path)
+    g <- function(k) ifelse(is.null(md$metadata[[k]]), NA, md$metadata[[k]])
     data.frame(
-        Title=md$metadata$`dc:title`,
+        Title=g('dc:title'),
         Path=path,
-        Size=get_dir_size(dirname(path)),
-        Citation=md$metadata$`dc:bibliographicCitation`,
-        ConformsTo=md$metadata$`dc:conformsTo`
+        Size=get_dir_size(ifelse(dir.exists(path), path, dirname(path))),
+        Citation=g('dc:bibliographicCitation'),
+        ConformsTo=g('dc:conformsTo')
     )
 }
-
