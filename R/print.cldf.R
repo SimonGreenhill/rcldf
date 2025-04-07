12dea0e75a50e821a23500bf82a16cfa44df79c2
SimonGreenhill <SimonGreenhill@users.noreply.github.com>
2025-04-08 08:37:15 +1200

    adding \value

--- R/print.cldf.R
+++ R/print.cldf.R
@@ -2,9 +2,9 @@
 #'
 #' @param x the CLDF dataset
 #' @param ... Arguments to be passed to or from other methods. Currently not used.
+#' @return No return value, called for side effects.
 #' @export
 #' @examples
-#' @return NULL
 #' cldfobj <- cldf(system.file("extdata/huon", "cldf-metadata.json", package = "rcldf"))
 #' print(cldfobj)
 print.cldf <- function(x, ...) {
