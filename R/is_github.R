f3b651289ecda1916e2b8394faf03e11f6b66ab0
SimonGreenhill <SimonGreenhill@users.noreply.github.com>
2025-09-14 11:13:00 +1200

    use urltools to make more robust

--- R/is_github.R
+++ R/is_github.R
@@ -2,9 +2,10 @@
 #'
 #' @param url A string
 #' @return A boolean TRUE/FALSE
+#' @importFrom urltools domain
 #' @export
 #' @examples
 #' is_github('https://github.com/SimonGreenhill/rcldf/')
 is_github <- function(url) {
-    is_url(url) && grepl("://github.com", url)
+    is_url(url) && urltools::domain(url) == "github.com"
 }
